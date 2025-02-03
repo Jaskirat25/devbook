2f44e1a009ecda83d5fe83f313e25408ad9a7da3
Jaskirat25 <jaskiratsingh251103@gmail.com>
2025-02-03 12:42:17 +0530

    components

--- src/actions.ts
+++ src/actions.ts
@@ -80,6 +80,47 @@ export const rePost = async (postId: number) => {
     });
   }
 };
+export const addComment = async (
+  prevState: { success: boolean; error: boolean },
+  formData: FormData
+) => {
+  const { userId } = await auth();
+
+  if (!userId) return { success: false, error: true };
+
+  const postId = formData.get("postId");
+  const username = formData.get("username");
+  const desc = formData.get("desc");
+
+  const Comment = z.object({
+    parentPostId: z.number(),
+    desc: z.string().max(140),
+  });
+
+  const validatedFields = Comment.safeParse({
+    parentPostId: Number(postId),
+    desc,
+  });
+
+  if (!validatedFields.success) {
+    console.log(validatedFields.error.flatten().fieldErrors);
+    return { success: false, error: true };
+  }
+
+  try {
+    await prisma.post.create({
+      data: {
+        ...validatedFields.data,
+        userId,
+      },
+    });
+    revalidatePath(`/${username}/status/${postId}`);
+    return { success: true, error: false };
+  } catch (err) {
+    console.log(err);
+    return { success: false, error: true };
+  }
+};
 export const savePost = async (postId: number) => {
   const { userId } = await auth();
 
